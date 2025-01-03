f80572d5e2b3ab59db76f287f04008b3ac1b3599
Batchuluun0605 <batchuluun655@gmail.com>
2025-01-02 22:30:24 -0800

    sidebar done

---
+++ src/components/ContentMain/ContentMain.jsx
@@ -0,0 +1,7 @@
+import "./contentMain.css";
+
+const ContentMain = () => {
+  return <div>ContentMain</div>;
+};
+
+export default ContentMain;
---
+++ src/components/ContentTop/ContentTop.jsx
@@ -0,0 +1,6 @@
+import "./contentTop.css";
+const ContentTop = () => {
+  return <div>ContentTop</div>;
+};
+
+export default ContentTop;
